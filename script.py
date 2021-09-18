762bc9006752cbc536ba50c4e51c4c27ab7df2b4
Davi Cintra <davi.cintra2002@gmail.com>
2021-09-18 05:00:02 -0300

    tuntsChallenge

---
+++ script.py
@@ -0,0 +1,78 @@
+# Importing CSV module
+
+from csv import reader
+
+# Importing gspread
+
+import gspread
+
+
+from oauth2client.service_account import ServiceAccountCredentials
+
+from math import ceil
+
+
+# Defining a main function
+
+def main():
+    # Allowing script to access the spreadsheet
+
+    scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
+            "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+
+    credentials = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
+    client = gspread.authorize(credentials)
+
+    # Accessing the CSV file
+
+    try:
+        spreadsheet = client.open_by_url("https://docs.google.com/spreadsheets/d/19Fdj2JvkE52SBI-PRyqHQcjvzrk-tsLKJ_tEZfp6CXQ/edit#gid=0")
+    except:
+        print("Couldn't reach CSV file!")
+
+    # Opening CVS file
+    worksheet = spreadsheet.get_worksheet(0)
+
+    # Defining total number of classes
+
+    classes = 50
+
+    for student_row in range(4, 28):
+        student_data = worksheet.row_values(student_row) 
+        print('Calculating situation of student number: ' + student_data[0])
+        average = calculate_average(student_data)
+        absences = int(student_data[2])
+
+        if(absences/classes >= 0.25):
+            worksheet.update_cell(student_row, 7, 'Reprovado por Falta')
+            worksheet.update_cell(student_row, 8, '0')
+        elif(average >= 7):
+            worksheet.update_cell(student_row, 7, 'Aprovado')
+            worksheet.update_cell(student_row, 8, '0')
+        elif(average >= 5 and average < 7):
+            naf = str(10/average)
+            worksheet.update_cell(student_row, 7, 'Exame Final')
+            worksheet.update_cell(student_row, 8, naf)
+
+
+        else:
+            worksheet.update_cell(student_row, 7, 'Reprovado por Nota')
+            worksheet.update_cell(student_row, 8, '0')
+    print("All Done!")
+
+
+
+
+# Defining calculate_average function:
+
+def calculate_average(student_data):
+    n1 = float(student_data[3])
+    n2 = float(student_data[4])
+    n3 = float(student_data[5])
+
+    return ceil((n1 + n2 + n3)/30)
+
+
+if __name__ == '__main__':
+    main()
+
